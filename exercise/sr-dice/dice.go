84cc3afb1bf5c22dc38332b60d8baab4be39fa78
Sean Perez <perez.josean@proton.me>
2026-09-01 18:28:35 +1100

    Update dice.go

--- exercise/sr-dice/dice.go
+++ exercise/sr-dice/dice.go
@@ -7,12 +7,42 @@ package main
 
 import (
 	"fmt"
+	random "math/rand/v2"
 )
 
-func getDiceSize() int {
-	var diceSides int
-	fmt.Scan(&diceSides)
-	return diceSides
+func getDiceDeets() map[string]int {
+	diceMap := map[string]int{
+		"diceSides":  0,
+		"diceRolls":  0,
+		"diceNumber": 0,
+	}
+	var input int
+	fmt.Println("How many dices are we using?")
+	fmt.Scan(&input)
+	diceMap["diceNumber"] = input
+	fmt.Println("How many times are you going to roll your dice(s)?")
+	fmt.Scan(&input)
+	diceMap["diceRolls"] = input
+	fmt.Println("How many sides do/does the dice(s) has/have?")
+	fmt.Scan(&input)
+	diceMap["diceSides"] = input
+	return diceMap
+}
+
+func rollTheDice(diceMap map[string]int) int {
+	
+	if diceMap["diceNumber"] == 1 {
+		return random.IntN(diceMap["diceSides"] + 1)
+	} else {
+		var total int
+		for dice := 1; dice <= diceMap["diceNumber"]; dice++ {
+
+			temp := random.IntN(diceMap["diceSides"] + 1)
+		fmt.Printf("Dice# %d is \n")
+		total += temp
+		}
+	}
+	return total
 }
 
 func main() {
@@ -27,5 +57,19 @@ func main() {
 	//
 	//--Notes:
 	//* Use packages from the standard library to complete the project
+	diceMap := getDiceDeets()
+	for roll := 0; roll < diceMap["diceRolls"]; roll++ {
+		fmt.Println("Roll #", roll)
+		fmt.Println("Rolling...")
 
+		total (LINK THE FUNCTION. CURRENTLY HERE. ALSO, TRY TO UNDERSTAND THE ROLL TOTAL.)
+
+		if TOTAL == 2 && diceMap["diceNumber"] == 2 {
+			fmt.Println("Snake eyes")
+		} else if {
+			
+		}
+	}
+
+	fmt.Println(diceMap)
 }
