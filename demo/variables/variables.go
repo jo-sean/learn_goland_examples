7ae19a364021da9e85098f9cfd1025b22462601e
Sean Perez <perez.josean@proton.me>
2026-09-01 12:52:44 +1100

    Adde demo and eidts of my own

--- demo/variables/variables.go
+++ demo/variables/variables.go
@@ -12,5 +12,21 @@ func main() {
 	fmt.Scan(&username)
 
 	fmt.Println("Your username is now: ", username)
+	var sum int
+	fmt.Println("The sum is", sum) // Initialize sum as 0.
 
+	part1, other := 1, 5
+	fmt.Println("part1 is", part1, "other is", other)
+
+	part2, other := 2, 0
+	fmt.Println("part1 is", part2, "other is", other)
+
+	sum = part1 + part2
+	fmt.Println("The sum is", sum)
+
+	var (
+		lessonName = "Variables"
+		lessonType = "Demo"
+	)
+	fmt.Printf("Lesson name is %s and lesson type is %s. \n", lessonName, lessonType) // Initialize sum as 0.
 }
