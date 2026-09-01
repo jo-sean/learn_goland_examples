0f501b5513de39ddf49275b920e62b6b21e68c82
Sean Perez <perez.josean@proton.me>
2026-09-01 12:29:37 +1100

    Updated variables demo

--- demo/variables/variables.go
+++ demo/variables/variables.go
@@ -5,4 +5,12 @@ import "fmt"
 func main() {
 	var myName = "Sean"
 	fmt.Println("My name is", myName)
+
+	var username string
+
+	fmt.Print("Type you username: ")
+	fmt.Scan(&username)
+
+	fmt.Println("Your username is now: ", username)
+
 }
