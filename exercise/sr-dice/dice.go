9dea0e62b6cbef5b49cc2bdd154a9d06f373368a
Sean Perez <perez.josean@proton.me>
2026-09-01 17:09:08 +1100

    Update dice.go

--- exercise/sr-dice/dice.go
+++ exercise/sr-dice/dice.go
@@ -2,21 +2,30 @@
 //  Create a program that can perform dice rolls. The program should
 //  report the results as detailed in the requirements.
 //
-//--Requirements:
-//* Print the sum of the dice roll
-//* Print additional information in these cirsumstances:
-//  - "Snake eyes": when the total roll is 2, and total dice is 2
-//  - "Lucky 7": when the total roll is 7
-//  - "Even": when the total roll is even
-//  - "Odd": when the total roll is odd
-//* The program must handle any number of dice, rolls, and sides
-//
-//--Notes:
-//* Use packages from the standard library to complete the project
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func getDiceSize() int {
+	var diceSides int
+	fmt.Scan(&diceSides)
+	return diceSides
+}
 
 func main() {
+	//--Requirements:
+	//* Print the sum of the dice roll
+	//* Print additional information in these cirsumstances:
+	//  - "Snake eyes": when the total roll is 2, and total dice is 2
+	//  - "Lucky 7": when the total roll is 7
+	//  - "Even": when the total roll is even
+	//  - "Odd": when the total roll is odd
+	//* The program must handle any number of dice, rolls, and sides
+	//
+	//--Notes:
+	//* Use packages from the standard library to complete the project
+
 }
