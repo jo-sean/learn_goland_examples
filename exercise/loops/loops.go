f385f2a45f28dcf225115b2f909f57e156e20a7e
Sean Perez <perez.josean@proton.me>
2026-09-01 16:32:05 +1100

    Update loops.go

--- exercise/loops/loops.go
+++ exercise/loops/loops.go
@@ -7,12 +7,26 @@
 //  - Print "Buzz" if the integer is divisible by 5
 //  - Print "FizzBuzz" if the integer is divisible by both 3 and 5
 //
-//--Notes:
-//* The remainder operator (%) can be used to determine divisibility
 
 package main
 
 import "fmt"
 
 func main() {
+	//--Requirements:
+	//* Print integers 1 to 50, except:
+	//  - Print "Fizz" if the integer is divisible by 3
+	//  - Print "Buzz" if the integer is divisible by 5
+	//  - Print "FizzBuzz" if the integer is divisible by both 3 and 5
+
+	for count := 1; count <= 50; count++ {
+		fmt.Println(count)
+		if count%3 == 0 && count%5 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if count%3 == 0 {
+			fmt.Println("Fizz")
+		} else if count%5 == 0 {
+			fmt.Println("Buzz")
+		}
+	}
 }
