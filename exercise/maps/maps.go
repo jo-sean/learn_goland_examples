31316e3dadb11c113be74ceea4e3de6159835386
Sean Perez <perez.josean@proton.me>
2026-09-03 15:19:39 +1100

    Update maps.go

--- exercise/maps/maps.go
+++ exercise/maps/maps.go
@@ -1,23 +1,12 @@
 //--Summary:
 //  Write a program to display server status.
 //
-//--Requirements:
-//* Create a function to print server status, including:
-//  - Number of servers
-//  - Number of servers for each status (Online, Offline, Maintenance, Retired)
-//* Store the existing slice of servers in a map
-//* Default all of the servers to `Online`
-//* Perform the following status changes and display server info:
-//  - display server info
-//  - change `darkstar` to `Retired`
-//  - change `aiur` to `Offline`
-//  - display server info
-//  - change all servers to `Maintenance`
-//  - display server info
 
 package main
 
-import "fmt"
+import {
+	"fmt"
+}
 
 const (
 	Online      = 0
@@ -26,6 +15,43 @@ const (
 	Retired     = 3
 )
 
+// * Create a function to print server status, including:
+//   - Number of servers
+//   - Number of servers for each status (Online, Offline, Maintenance, Retired)
+func printServerStatus(serverMap *map[string]int) {
+	fmt.Println("You have", len(*serverMap), "servers.")
+	//fmt.Println()
+}
+
+func serverInitialization(serverMap *map[string]int, servers *[]string) {
+	for _, server := range *servers {
+		status, found := serverMap[server]
+	if !found {
+		fmt.Println("Adding", server, "and turning it oneline.")
+		serverMap[server] = Online
+
+	} else {
+		fmt.Println("You already have the server named", server, "and its status is currently", status)
+	}
+
+	}
+}
+
+// --Requirements:
+// * Store the existing slice of servers in a map
+// * Default all of the servers to `Online`
+// * Perform the following status changes and display server info:
+//   - display server info
+//   - change `darkstar` to `Retired`
+//   - change `aiur` to `Offline`
+//   - display server info
+//   - change all servers to `Maintenance`
+//   - display server info
 func main() {
 	servers := []string{"darkstar", "aiur", "omicron", "w359", "baseline"}
+
+	serverMap := make(map[string]int)
+
+	serverInitialization(&serverMap, &servers)
+
 }
