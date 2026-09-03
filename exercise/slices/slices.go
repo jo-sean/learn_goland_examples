c67fc7ca97e2a84a81dc173821f436e31ef671e6
Sean Perez <perez.josean@proton.me>
2026-09-03 15:02:53 +1100

    Update slices.go

--- exercise/slices/slices.go
+++ exercise/slices/slices.go
@@ -1,16 +1,6 @@
 //--Summary:
 //  Create a program to manage parts on an assembly line.
 //
-//--Requirements:
-//* Using a slice, create an assembly line that contains type Part
-//* Create a function to print out the contents of the assembly line
-//* Perform the following:
-//  - Create an assembly line having any three parts
-//  - Add two new parts to the line
-//  - Slice the assembly line so it contains only the two new parts
-//  - Print out the contents of the assembly line at each step
-//--Notes:
-//* Your program output should list 3 parts, then 5 parts, then 2 parts
 
 package main
 
@@ -18,25 +8,36 @@ import "fmt"
 
 type Part string
 
+// * Create a function to print out the contents of the assembly line
 func printAssemblyLine(assemblyLine *[]Part) {
 	for i, value := range *assemblyLine {
-		fmt.Println(i, "- Item on the assembly is: ", value)
+		fmt.Println("Item", i, "on the assembly is: ", value)
 	}
 }
 
+// --Requirements:
+// * Using a slice, create an assembly line that contains type Part
+// * Perform the following:
+//   - Create an assembly line having any three parts
+//   - Add two new parts to the line
+//   - Slice the assembly line so it contains only the two new parts
+//   - Print out the contents of the assembly line at each step
 func main() {
-	var assemblyLine []Part
+	assemblyLine := make([]Part, 3)
 
-	var first Part = "First"
-	var second Part = "Second"
-	var third Part = "Third"
-	// var fourth Part = "Fourth"
-	// var fifth Part = "Fifth"
-
-	assemblyLine[0] = first
-	assemblyLine[1] = second
-	assemblyLine[2] = third
+	assemblyLine[0] = "Nuts"
+	assemblyLine[1] = "Bolts"
+	assemblyLine[2] = "Rivets"
 
 	printAssemblyLine(&assemblyLine)
+
+	fmt.Println()
+
+	assemblyLine = append(assemblyLine, "Washers", "Screws")
+	printAssemblyLine(&assemblyLine)
+
+	fmt.Println()
+	newItemsOnly := assemblyLine[3:]
+	printAssemblyLine(&newItemsOnly)
 
 }
