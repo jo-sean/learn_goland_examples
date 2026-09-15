973646d80fc74618739aa5503506d61ce27139dc
Sean Perez <perez.josean@proton.me>
2026-09-15 11:20:15 +1100

    Update rcv-func_test.go

--- exercise/testing/rcv-func_test.go
+++ exercise/testing/rcv-func_test.go
@@ -17,40 +17,34 @@ import (
 	"testing"
 )
 
-func maximumLimitHealthEnergyCheck(t testing.T) {
+func TestMaximumLimitHealthEnergyCheck(t *testing.T) {
 	bob := NewPlayer("Robert")
 
-	for i := 0; i < 1; i++ {
-		bob.Heal(Potion)
-		if bob.health > bob.maxHealth {
-			t.Errorf("%s has %d health. However, Max Health is %d", bob.name, bob.health, bob.maxHealth)
+	bob.Heal(Potion * 2)
+	if bob.health > bob.maxHealth {
+		t.Errorf("%s has %d health. However, Max Health is %d", bob.name, bob.health, bob.maxHealth)
 
-		}
 	}
 
-	for i := 0; i < 1; i++ {
-		bob.RestoreMagic(Ether)
-		if bob.energy > bob.maxEnergy {
-			t.Errorf("%s has %d energy. However, Max Energy is %d", bob.name, bob.energy, bob.maxEnergy)
-		}
+	bob.RestoreMagic(Ether * 2)
+	if bob.energy > bob.maxEnergy {
+		t.Errorf("%s has %d energy. However, Max Energy is %d", bob.name, bob.energy, bob.maxEnergy)
 	}
+
 }
 
-func minimumLimitHealthEnergyCheck(t testing.T) {
+func TestMinimumLimitHealthEnergyCheck(t *testing.T) {
 	bob := NewPlayer("Robert")
 
-	for i := 0; i < 2; i++ {
-		bob.Damage(Potion)
-		if bob.health < 0 {
-			t.Errorf("%s has %d health. That is less than zero", bob.name, bob.health)
+	bob.Damage(Potion * 2)
+	if bob.health < 0 {
+		t.Errorf("%s has %d health. That is less than zero", bob.name, bob.health)
 
-		}
 	}
 
-	for i := 0; i < 2; i++ {
-		bob.RestoreMagic(Ether)
-		if bob.energy < 0 {
-			t.Errorf("%s has %d energy. That is less than zero", bob.name, bob.energy)
-		}
+	bob.UseMagic(Ether * 2)
+	if bob.energy < 0 {
+		t.Errorf("%s has %d energy. That is less than zero", bob.name, bob.energy)
 	}
+
 }
