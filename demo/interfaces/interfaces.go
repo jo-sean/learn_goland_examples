e9c3d0ef40d52be0f7eb4913ebfff42c253fa9ad
Sean Perez <perez.josean@proton.me>
2026-09-16 11:23:50 +1100

    Update interfaces.go

--- demo/interfaces/interfaces.go
+++ demo/interfaces/interfaces.go
@@ -2,6 +2,32 @@ package main
 
 import "fmt"
 
+type Preparer interface {
+	PrepareDish()
+}
+
+type Chicken string
+type Salad string
+
+func (c Chicken) PrepareDish() {
+	fmt.Println("Cook chicken")
+}
+
+func (s Salad) PrepareDish() {
+	fmt.Println("Chop salad")
+	fmt.Println("Add dressing")
+}
+func prepareDishes(dishes []Preparer) {
+	fmt.Println("Prepare dishes:")
+	for _, dish := range dishes {
+		fmt.Printf("--Dish: %v--\n", dish)
+		dish.PrepareDish()
+	}
+	fmt.Println()
+}
+
 func main() {
+	dishes := []Preparer{Chicken("Chicken Wings"), Salad("Ceasar Salad")}
+	prepareDishes(dishes)
 
 }
